2e0cf369ed8cfe80ae8d2863544e068c134d9552
rakotoniainafanomezantsoajeana <rakotoniainafanomezantsoajeana@gmail.com>
2026-08-27 15:52:14 +0300

    ajout de host

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
+    allowedHosts: [
+      'localhost',
+      'chat.tracemada.net',
+    ]
   },
   preview: {
     host: true,
